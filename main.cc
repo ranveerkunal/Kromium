ef06f29542a69ab1986df90184b70c8855a6db7d
Mojo Singh <mojo@layla.(none)>
2009-06-17 10:31:18 +0530

    Indentation

--- main.cc
+++ main.cc
@@ -12,36 +12,36 @@
 
 int main (int argc, char *argv[])
 {
-    KAboutData aboutData(
-                         // The program name used internally.
-                         "tutorial1",
-                         // The message catalog name
-                         // If null, program name is used instead.
-                         0,
-                         // A displayable program name string.
-                         ki18n("Tutorial 1"),
-                         // The program version string.
-                         "1.0",
-                         // Short description of what the app does.
-                         ki18n("Displays a KMessageBox popup"),
-                         // The license this code is released under
-                         KAboutData::License_GPL,
-                         // Copyright Statement
-                         ki18n("(c) 2007"),
-                         // Optional text shown in the About box.
-                         // Can contain any information desired.
-                         ki18n("Some text..."),
-                         // The program homepage string.
-                         "http://tutorial.com/",
-                         // The bug report email address
-                         "submit@bugs.kde.org");
+  KAboutData aboutData(
+      // The program name used internally.
+      "tutorial1",
+      // The message catalog name
+      // If null, program name is used instead.
+      0,
+      // A displayable program name string.
+      ki18n("Tutorial 1"),
+      // The program version string.
+      "1.0",
+      // Short description of what the app does.
+      ki18n("Displays a KMessageBox popup"),
+      // The license this code is released under
+      KAboutData::License_GPL,
+      // Copyright Statement
+      ki18n("(c) 2007"),
+      // Optional text shown in the About box.
+      // Can contain any information desired.
+      ki18n("Some text..."),
+      // The program homepage string.
+      "http://tutorial.com/",
+      // The bug report email address
+      "submit@bugs.kde.org");
 
-    KCmdLineArgs::init( argc, argv, &aboutData );
-    KApplication app;
-    KGuiItem yesButton( i18n( "Hello" ), QString(),
-                        i18n( "This is a tooltip" ),
-                        i18n( "This is a WhatsThis help text." ) );
-    KMessageBox::questionYesNo( 0, i18n( "Hello World" ),
-                                i18n( "Hello" ), yesButton );
-    return 0;
+  KCmdLineArgs::init( argc, argv, &aboutData );
+  KApplication app;
+  KGuiItem yesButton( i18n( "Hello" ), QString(),
+      i18n( "This is a tooltip" ),
+      i18n( "This is a WhatsThis help text." ) );
+  KMessageBox::questionYesNo( 0, i18n( "Hello World" ),
+      i18n( "Hello" ), yesButton );
+  return 0;
 }
